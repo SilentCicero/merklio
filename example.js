959a945e3f6ffcf5d3785ced3d41973bd8b74911
silentcicero <thenickdodson@gmail.com>
2019-06-13 18:33:13 +0100

    cool

--- example.js
+++ example.js
@@ -4,9 +4,7 @@ This is the Best-Case example of the service!
 Add a new hash to be merklized
 */
 
-axios.post('https://merkl.io/add/', {
-  hash: '0x26b74a107f953ab5e3aac2dcde97126224fe6c7da163782bba6372b3deaf1a14',
-});
+axios.post('https://api.merkl.io/add/0x26b74a107f953ab5e3aac2dcde97126224fe6c7da163782bba6372b3deaf1a14');
 
 // timestamp serer signature
 
@@ -15,11 +13,15 @@ axios.post('https://merkl.io/add/', {
   timestamp: 190234824, // timestamp submitted
 }
 
+// signature proof from server (using ethers.js below)
+// server address: 0x5356E51BD23c816893F75aAd10FF656bb3479a07
+// digest recover: '0x5356E51BD23c816893F75aAd10FF656bb3479a07' === utils.recoverAddress(utils.keccak256(utils.toUtf8Bytes(JSON.stringify({ hash, timestamp })))), signature)
+
 /*
 Get hash status or proof
 */
 
-axios.get('https://merkl.io/status/0x26b74a107f953ab5e3aac2dcde97126224fe6c7da163782bba6372b3deaf1a14');
+axios.get('https://api.merkl.io/status/0x26b74a107f953ab5e3aac2dcde97126224fe6c7da163782bba6372b3deaf1a14');
 
 // returns this
 
@@ -30,8 +32,10 @@ axios.get('https://merkl.io/status/0x26b74a107f953ab5e3aac2dcde97126224fe6c7da16
 // or
 
 {
+  hash: '0x26b74a107f953ab5e3aac2dcde97126224fe6c7da163782bba6372b3deaf1a14',
+  transactionHash: '0xce4918a34258835278c9e4bbc6a653e4635a1727567e71bb5f5bc90cd4182fff',
   status: 'transacted',
-  masterHash: '0x26b74a107f953ab5e3aac2dcde97126224fe6c7da163782bba6372b3deaf1a14',
+  created: '',
   proof: {
     '0xfb4918a34258835278c9e4bbc6a653e4635a1727567e71bb5f5bc90cd4182fbf': {
       '0x11105bb2d695e056d31777f0ce726de0335ff66a3f66ece9ab91f7b89a788bc2': [
